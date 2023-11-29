9bccaceae195d5b47d9868be1c74e5a390640cac
Chase Wrenn <chasewrenn@ufl.edu>
2023-11-28 19:08:57 -0500

    Create and add basic functionality to OptimisticallyUnchokedNeighborHandler.java

---
+++ src/OptimisticallyUnchokedNeighborHandler.java
@@ -0,0 +1,33 @@
+import java.util.*;
+
+public class OptimisticallyUnchokedNeighborHandler {
+    private Vitals vitals;
+    private Random random;
+
+    //TODO: Not sure if there needs to be more implemented to insure this is integrated with the rest of the code
+
+    public void initOptimisticallyUnchokedNeighborHandler() {
+        try {
+            HashMap<Integer, PeerWorker> interestedPeers = this.vitals.getInterestedWorkers();
+            HashMap<Integer, PeerWorker> unchokedPeers = this.vitals.getUnchokedPeers();
+            List<PeerWorker> candidates = new ArrayList<>();
+
+            // Filter for interested but choked peers
+            for (Map.Entry<Integer, PeerWorker> entry : interestedPeers.entrySet()) {
+                if (!unchokedPeers.containsKey(entry.getKey())) {
+                    candidates.add(entry.getValue());
+                }
+            }
+
+            // Randomly select one peer to be optimistically unchoked
+            if (!candidates.isEmpty()) {
+                PeerWorker selectedPeer = candidates.get(random.nextInt(candidates.size()));
+                selectedPeer.sendUnchokeMessage();
+                this.vitals.setOptimisticallyUnchokedPeer(selectedPeer.getPeerId());
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
